8520c5dbb2d1dbdcef3657e3bdb81245f6cc56ca
Lisandro Luna <f.lisandro.luna@gmail.com>
2022-07-18 02:23:49 -0300

    web

--- apps/frontend/js/index.js
+++ apps/frontend/js/index.js
@@ -1,12 +1,16 @@
 const express = require('express');
 
 const app = express();
+app.set('views', './')
+app.set('view engine', 'pug')
 
 app.get('/', (req, res) => {
-   
-  res.send('pong');
-});
-
+  let info
+  fetch('http://backend.app-backend.svc.cluster.local:8080/Spring4-1/data/person?id=15')
+  .then(response => response.json())
+  .then(data => info = data); 
+  res.render('index', { title: 'Demo Frontend', name: data.name, location: data.location, page: 'Datos:' })
+})
 app.listen(80, () => {
   console.log('listening on port 80');
 });
